402bfe04cbfcf4805bf904f8c45f5a9f68d640c1
Michael Mason <TeaUponTweed@gmail.com>
2019-03-29 23:11:03 -0600

    kinda works

--- rotate_simple.py
+++ rotate_simple.py
@@ -1,7 +1,9 @@
+import sys
 import itertools
-import argparse
 
 import numpy as np
+from scipy.ndimage.interpolation import map_coordinates
+
 import imutils
 import cv2
 import matplotlib
@@ -12,6 +14,9 @@ import matplotlib.cm as cm
 from skimage.filters import scharr
 from skimage.draw import line_aa
 from skimage.feature import peak_local_max
+from skimage.measure import block_reduce
+
+from numba import njit, jit
 
 def resize(im):
     desired_size = max(im.shape)
@@ -39,7 +44,7 @@ def find_intercept(l1, l2):
         assert np.isclose(y1+m*dy1, y2+n*dy2)
         return x1+m*dx1, y1+m*dy1
 
-
+# @njit
 def map_uv_to_xy(u, v, P0, P1, P2, P3):
     # find pointing unit vectors assuming clock-wise points
     r0 = (P1 - P0)/np.linalg.norm(P1 - P0)
@@ -55,45 +60,73 @@ def map_uv_to_xy(u, v, P0, P1, P2, P3):
     A=N0[0]
     B=N0[1]
     C=-P0@N0
-    D=N0[0]+𝑁2[0]
-    E=N0[1]+𝑁2[1]
+    D=N0[0]+N2[0]
+    E=N0[1]+N2[1]
     F=-P0@N0-P2@N2
+
     G=N1[0]
     H=N1[1]
     I=-P0@N1
     J=N1[0]+N3[0]
     K=N1[1]+N3[1]
-    J=-P0@N1-P2@N3
+    L=-P0@N1-P2@N3
 
     uDA=u*(D-A)
     uEB=u*(E-B)
     uFC=u*(F-C)
+
     vJG=v*(J-G)
     vKH=v*(K-H)
-    vJG=v*(J-G)
-
-    x=vKH@uFC-vLI@uEB - vJG@uEB-vKH@uDA
-    y=vLI@uDA-uFC@vJG - vJG@uEB-vKH@uDA
+    vLI=v*(L-I)
+    print(vJG*uEB)
+    print(vKH*uDA)
+    print('***')
+    if np.isclose(vJG*uEB-vKH*uDA, 0):
+        return 0, 0
+    x=(vKH*uFC-vLI*uEB)/(vJG*uEB-vKH*uDA)
+    y=(vLI*uDA-uFC*vJG)/(vJG*uEB-vKH*uDA)
     return x, y
 
+# @njit
+def map_uv_to_xy(u, v, P0, P1, P2, P3):
+    # find pointing unit vectors assuming clock-wise points
+    r0 = (P1 - P0)/np.linalg.norm(P1 - P0)
+    r1 = (P2 - P1)/np.linalg.norm(P2 - P1)
+    r2 = (P3 - P2)/np.linalg.norm(P3 - P2)
+    r3 = (P0 - P3)/np.linalg.norm(P0 - P3)
+    # rotate by 90deg to get normal vectors inward
+    N0 = -np.array([-r0[1],r0[0]])
+    N1 = -np.array([-r1[1],r1[0]])
+    N2 = -np.array([-r2[1],r2[0]])
+    N3 = -np.array([-r3[1],r3[0]])
+
+    # x = (P0[0]*N0[0] - u*(P0[0]*N0[0]+P2[0]*N2[0]))/(N0[0]-u*(N0[0]+N2[0])) + (P0[0]*N1[0] - v*(P0[0]*N1[0]+P2[0]*N3[0]))/(N1[0]-v*(N1[0]+N3[0]))
+    # y = (P0[1]*N0[1] - u*(P0[1]*N0[1]+P2[1]*N2[1]))/(N0[1]-u*(N0[1]+N2[1])) + (P0[1]*N1[1] - v*(P0[1]*N1[1]+P2[1]*N3[1]))/(N1[1]-v*(N1[1]+N3[1]))
+    A = np.array([
+        [N0[0]-u*(N0[0]+N2[0]), N1[0]-v*(N1[0]+N3[0])],
+        [N0[1]-u*(N0[1]+N2[1]), N1[1]-v*(N1[1]+N3[1])]
+    ])
+    b = np.array([
+        P0[0]*N0[0] - u*(P0[0]*N0[0]+P2[0]*N2[0]) + P0[0]*N1[0] - v*(P0[0]*N1[0]+P2[0]*N3[0]),
+        P0[1]*N0[1] - u*(P0[1]*N0[1]+P2[1]*N2[1]) + P0[1]*N1[1] - v*(P0[1]*N1[1]+P2[1]*N3[1]),
+    ])
+    return np.linalg.solve(A, b)
+    # return x, y
+
 def main():
-    # construct the argument parse and parse the arguments
-    ap = argparse.ArgumentParser()
-    ap.add_argument("-i", "--image", required=True,
-        help="path to the image file")
-    args = vars(ap.parse_args())
-
-    image = cv2.imread(args["image"])
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY).astype(np.float32)
-
+    image = cv2.imread(sys.argv[1])
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY).astype(np.float32)
+    # TODO scale down image intelligently
+    gray = block_reduce(gray, (8, 8))
+    print(gray.shape)
     im_rows,im_cols = gray.shape
     edged = scharr(gray)
- 
     original_edged = edged
     edged = resize(edged)
 
     rows,cols = edged.shape
-    angles = np.arange(0, 185, 1.0)
+    angles = np.arange(0, 185, 0.5)
+
     data = np.zeros((len(angles), cols))
     for i, angle in enumerate(angles):
         rotated = imutils.rotate(edged, angle)
@@ -102,10 +135,9 @@ def main():
         for j in range(cols):
             data[i, j] += projection[j]
 
-    # TODO make thresh dependednt on image size?
-    xy = peak_local_max(data, min_distance=5,threshold_abs=np.max(data)*.3)
+    # TODO scale threshold dynamically to get a reasonable number of detections
+    xy = peak_local_max(data, min_distance=5,threshold_abs=np.max(data)*.4)
 
-    asd = np.arange(-150, 150, 1)
     lines = []
     for (rotation, extent) in xy:
         score = data[rotation, extent]
@@ -125,7 +157,6 @@ def main():
         lines.append((x,y,dx,dy))
 
     intersection_points = []
-    scores = []
     for l1, l2 in itertools.combinations(lines, 2):
         x, y = find_intercept(l1, l2)
         if x < 0 or x >= im_cols:
@@ -136,51 +167,76 @@ def main():
 
     def gen_scored_points():
         for points in itertools.combinations(intersection_points, 4):
-            points = sorted(points, key=lambda x: -sum(x))
-            sorted_points = [np.array(points.pop()[:2])]
+            points = list(map(np.array, sorted(points, key=lambda x: -sum(x))))
+            sorted_points = [points.pop()[:2]]
             while len(points) > 0:
-                points.sort(key=lambda x: -np.linalg.norm(np.array(x)-sorted_points[-1]))
+                points.sort(key=lambda x: -min(np.abs(np.array(x)-sorted_points[-1])))
                 sorted_points.append(points.pop())
 
             sorted_points = [np.round(p).astype(int) for p in sorted_points]
             score = 0
-            last_delta = sorted_points[0] - sorted_points[-1]
-            test = np.copy(original_edged)
-
+            deltas = [sorted_points[0] - sorted_points[-1]]
             for i in range(4):
                 p1 = sorted_points[i]
                 p2 = sorted_points[(i+1)%4]
 
                 delta = p2 - p1
-                # print('|delta|=',np.linalg.norm(delta))
                 # filter out small edges
+                # print('|delta|=',np.linalg.norm(delta))
                 if np.linalg.norm(delta) < min(im_rows, im_cols)/2:
                     score = 0
                     break
-                # filter out acute angles
-                if last_delta is not None:
-                    angle = np.degrees(np.arccos(delta@last_delta/np.linalg.norm(delta)/np.linalg.norm(last_delta)))
-                    # print('angle=', angle)
-                    if angle < 70 or angle > 110:
-                        score = 0
-                        break
 
-                last_delta = delta
+                # filter out acute angles
+                angle = np.degrees(np.arccos(delta@deltas[-1]/np.linalg.norm(delta)/np.linalg.norm(deltas[-1])))
+                # print('angle=', angle)
+                if angle < 60 or angle > 120:
+                    score = 0
+                    break
 
                 rr,cc,vals =line_aa(p1[1], p1[0], p2[1], p2[0])
                 ix_1 = rr < im_rows-1
                 ix_2 = cc < im_cols-1
                 ix = ix_1 * ix_2
                 score += np.sum(vals[ix] * original_edged[rr[ix], cc[ix]])
+                deltas.append(delta)
 
-            yield score, sorted_points 
+            yield score, sorted_points, deltas
 
 
-    score, sorted_points = max(gen_scored_points(), key=lambda x: x[0])
+    score, sorted_points, deltas = max(gen_scored_points(), key=lambda x: x[0])
     print(sorted_points)
-    plt.imshow(image, cmap=cm.jet, interpolation='nearest')
+    plt.imshow(gray, cmap=cm.gray, interpolation='nearest')
     print('score=',score)
-    plt.plot(*zip(*(sorted_points+[sorted_points[0]])), linestyle=':', marker='', color='k')
+    plt.plot(*zip(*(sorted_points+[sorted_points[0]])), linestyle=':', marker='', color='r')
+    plt.show()
+
+    # undistort image
+    width_over_height = 11/8.5
+    width_pixels = int(np.round(max(map(np.linalg.norm, deltas)))) # assumes wide image
+    height_pixels = int(np.round(max(map(np.linalg.norm, deltas))/width_over_height))
+    # print(width_pixels)
+    # print(height_pixels)
+    # to_interp = np.zeros((height_pixels, width_pixels, 2))
+    wakka = [p.astype(float) for p in sorted_points]
+    out = np.zeros((height_pixels, width_pixels))
+    for i in range(height_pixels):
+        # print(i)
+        to_interp = []
+
+        for j in range(width_pixels):
+            x, y = map_uv_to_xy(i/height_pixels, j/width_pixels, *wakka)
+            # x, y = blarg(j/width_pixels, i/height_pixels, *wakka)
+            # print(x, y)
+            to_interp.append([x, y])
+            # to_interp[i, j, :] = (x, y)
+        # print(to_interp)
+        # to_interp = np.array(to_interp)
+        # print(to_interp.shape)
+        # print(gray.shape)
+
+        out[i,:] = map_coordinates(gray, list(zip(*to_interp)), order=1)
+    plt.imshow(out)
     plt.show()
 
 
