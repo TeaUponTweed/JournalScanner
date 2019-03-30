8aadc1ee7945b582f877464735d17d6b67597053
Michael Mason <TeaUponTweed@gmail.com>
2019-03-30 00:02:12 -0600

    reformulate

--- rotate_simple.py
+++ rotate_simple.py
@@ -88,7 +88,7 @@ def map_uv_to_xy(u, v, P0, P1, P2, P3):
     return x, y
 
 # @njit
-def map_uv_to_xy(u, v, P0, P1, P2, P3):
+def find_normals(P0, P1, P2, P3):
     # find pointing unit vectors assuming clock-wise points
     r0 = (P1 - P0)/np.linalg.norm(P1 - P0)
     r1 = (P2 - P1)/np.linalg.norm(P2 - P1)
@@ -99,6 +99,10 @@ def map_uv_to_xy(u, v, P0, P1, P2, P3):
     N1 = np.array([-r1[1],r1[0]])
     N2 = np.array([-r2[1],r2[0]])
     N3 = np.array([-r3[1],r3[0]])
+    return N0, N1, N2, N3
+
+def map_uv_to_xy(u, v, P0, P1, P2, P3, N0, N1, N2, N3):
+
 
     # x = (P0[0]*N0[0] - u*(P0[0]*N0[0]+P2[0]*N2[0]))/(N0[0]-u*(N0[0]+N2[0])) + (P0[0]*N1[0] - v*(P0[0]*N1[0]+P2[0]*N3[0]))/(N1[0]-v*(N1[0]+N3[0]))
     # y = (P0[1]*N0[1] - u*(P0[1]*N0[1]+P2[1]*N2[1]))/(N0[1]-u*(N0[1]+N2[1])) + (P0[1]*N1[1] - v*(P0[1]*N1[1]+P2[1]*N3[1]))/(N1[1]-v*(N1[1]+N3[1]))
@@ -107,8 +111,8 @@ def map_uv_to_xy(u, v, P0, P1, P2, P3):
         [(1-u)*N0[1] - u*N2[1], (1-v)*N1[1] - v*N3[1]]
     ])
     b = np.array([
-        (1-u)*P0[0]*N0[0] - u*P2[0]*N2[0] + (1-v)*P0[0]*N1[0] - v*P2[0]*N3[0],
-        (1-u)*P0[1]*N0[1] - u*P2[1]*N2[1] + (1-v)*P0[1]*N1[1] - v*P2[1]*N3[1],
+        (1-u)*P0[0]*N0[0] - u*P2[0]*N2[0] + (1-u)*P0[1]*N0[1] - u*P2[1]*N2[1],
+        (1-v)*P0[1]*N1[1] - v*P2[1]*N3[1] + (1-v)*P0[0]*N1[0] - v*P2[0]*N3[0],
     ])
     return np.linalg.solve(A.T, b)
     # return x, y
@@ -209,7 +213,7 @@ def main():
     plt.imshow(gray, cmap=cm.gray, interpolation='nearest')
     print('score=',score)
     plt.plot(*zip(*(sorted_points+[sorted_points[0]])), linestyle=':', marker='', color='r')
-    plt.show()
+    # plt.show()
 
     # undistort image
     width_over_height = 11/8.5
@@ -220,15 +224,29 @@ def main():
     # to_interp = np.zeros((height_pixels, width_pixels, 2))
     wakka = [p.astype(float) for p in sorted_points]
     out = np.zeros((height_pixels, width_pixels))
+    normals = find_normals(*wakka)
+    for i in range(4):
+        base = wakka[i]+deltas[(i+1)%4]/2
+        # print(normals[i])
+        # xy =  # + normals[i]*np.array([0, 10])
+        # print(xy)
+
+        plt.plot(
+            [base[0], base[0] + normals[i][0]*10],
+            [base[1], base[1] + normals[i][1]*10],
+            color='k'
+        )
+    plt.show()
+    # plt.imshow
     for i in range(height_pixels):
         # print(i)
         to_interp = []
 
         for j in range(width_pixels):
-            x, y = map_uv_to_xy(i/height_pixels, j/width_pixels, *wakka)
+            x, y = map_uv_to_xy(i/height_pixels, j/width_pixels, *wakka, *normals)
             # x, y = blarg(j/width_pixels, i/height_pixels, *wakka)
             # print(x, y)
-            to_interp.append([x, y])
+            to_interp.append([y, x])
             # to_interp[i, j, :] = (x, y)
         # print(to_interp)
         # to_interp = np.array(to_interp)
