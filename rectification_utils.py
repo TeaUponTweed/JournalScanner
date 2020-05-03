b618a246e76993871edd1c3acc29ab776c6600c2
Michael Mason <TeaUponTweed@gmail.com>
2020-05-03 10:08:21 -0600

    fix bugs in rectifiaction step + add some tests

--- rectification_utils.py
+++ rectification_utils.py
@@ -1,9 +1,8 @@
 import math
 
-# import matplotlib.pyplot as plt
-# from numba import njit
 import numpy as np
 import scipy as sp
+from shapely.geometry import Polygon, Point
 
 def two_norm(a):
     return math.sqrt(a[0]*a[0] + a[1]*a[1])
@@ -14,72 +13,29 @@ def find_normals(P):
     r21 = (P[2] - P[1])/two_norm(P[2] - P[1])
     r32 = (P[3] - P[2])/two_norm(P[3] - P[2])
     r03 = (P[0] - P[3])/two_norm(P[0] - P[3])
-    # rotate by 90deg to get normal vectors inward
+    # rotate by -90 deg to get normal vectors inward
     N0 = np.array([r03[1],-r03[0]])
     N1 = np.array([r10[1],-r10[0]])
     N2 = np.array([r21[1],-r21[0]])
     N3 = np.array([r32[1],-r32[0]])
-    # N0 = [-r03[1],r03[0]]
-    # N1 = [-r10[1],r10[0]]
-    # N2 = [-r21[1],r21[0]]
-    # N3 = [-r32[1],r32[0]]
-    # N0 = [-r03[0], -r03[1]]
-    # N1 = [-r10[0], -r10[1]]
-    # N2 = [-r21[0], -r21[1]]
-    # N3 = [-r32[0], -r32[1]]
+
     return (N0, N1, N2, N3)
 
 
-# @njit
 def map_uv_to_xy(u, v, P, N):
     A = np.zeros((2,2))
     b = np.zeros(2)
-    # print(N[2])
-    # print(u)
-    # print(u*N[2])
-    # print((1-u))
-    # print(N[0])
     A[0, :] = u*N[2]-(1-u)*N[0]
     A[1, :] = v*N[3]-(1-v)*N[1]
     b[0] = u*P[2]@N[2]-(1-u)*P[0]@N[0]
     b[1] = v*P[3]@N[3]-(1-v)*P[0]@N[1]
     return np.linalg.solve(A, b)
 
-    '''
-    A[0, :] = u*N[2]-(1-u)*N[0]
-    A[1, :] = v*N[3]-(1-v)*N[1]
-    b[0] = u*P[2]@N[2]-(1-u)*P[0]@N[0]
-    b[1] = v*P[3]@N[3]-(1-v)*P[0]@N[1]
-    return np.linalg.solve(A, b)
-    '''
-    nu = 1 - u
-    nv = 1 - v
-    '''
-    A =
-    u*N[2][0]-nu*N[0][0]  u*N[2][1]-nu*N[0][1]
-    v*N[3][0]-nv*N[1][0]  v*N[3][1]-nv*N[1][1]
-
-    A_inv =
-     v*N[3][1]-nv*N[1][1]  -u*N[2][1]+nu*N[0][1]
-    -v*N[3][0]+nv*N[1][0]   u*N[2][0]-nu*N[0][0]
-    
-    '''
-    b_0 = u*(P[2][0]*N[2][0] + P[2][1]*N[2][1])-nu*(P[0][0]*N[0][0] + P[0][1]*N[0][1])
-    b_1 = v*(P[3][0]*N[3][0] + P[3][1]*N[3][1])-nv*(P[0][0]*N[1][0] + P[0][1]*N[1][1])
-    x = b_0 * ( v*N[3][1]-nv*N[1][1]) + b_1*(-u*N[2][1]+nu*N[0][1])
-    y = b_0 * (-v*N[3][0]+nv*N[1][0]) + b_1*( u*N[2][0]-nu*N[0][0])
-    # print(u,v,'->',int(x),int(y))
-    return x, y
-
-# @njit
 def get_sample_xy_points(points, normals, npoints, height_pixels, width_pixels):
     out_x = np.zeros((npoints, npoints))
     out_y = np.zeros((npoints, npoints))
     u = np.linspace(0, 1, npoints)
     v = np.linspace(0, 1, npoints)
-    # print(points)
-    # print(u)
-    # print(v)
     for i in range(npoints):
         for j in range(npoints):
             xy = map_uv_to_xy(u[j], v[i], points, normals)
@@ -88,18 +44,15 @@ def get_sample_xy_points(points, normals, npoints, height_pixels, width_pixels):
             if r < height_pixels:
                 out_y[i,j] = r  
             else:
-                # assert False
                 out_y[i,j] = height_pixels - 1
 
             if c < width_pixels:
                 out_x[i,j] = c
             else:
-                # assert False
                 out_x[i,j] = width_pixels - 1
 
     return u, v, out_x, out_y
 
-# @njit
 def get_square_impl(xx, yy, gray):
     out = np.empty(gray.shape, gray.dtype)
     for i in range(xx.shape[0]):
@@ -111,23 +64,11 @@ def get_square_image(gray, width_pixels, height_pixels, points):
     normals = find_normals(points)
     out = np.zeros((height_pixels, width_pixels))
     u, v, x_map, y_map = get_sample_xy_points(points, normals, 30, gray.shape[0], gray.shape[1])
-    # u, v, x_map, y_map = get_sample_xy_points(points, normals, 30, height_pixels, width_pixels)
-    # print(u)
-    # print(v)
-    # print(x_map)
-    # print(y_map)
-    # plt.imshow(out, )
-    # plt.imshow(gray, cmap='gray', vmin=0, vmax=255)
-    # plt.plot(x_map.flatten(), y_map.flatten(), marker='x', color='k')
-    # plt.show()
+
     x_func = sp.interpolate.interp2d(u, v, x_map)
     y_func = sp.interpolate.interp2d(u, v, y_map)
     u = np.linspace(0,1,width_pixels)
     v = np.linspace(0,1,height_pixels)
-    # plt.imshow(gray, cmap='gray', vmin=0, vmax=255)
-    # plt.plot(x_map.flatten(), y_map.flatten(), marker='x', color='k')
-    # plt.show()
-    # uu, vv = np.meshgrid(u, v)
 
     xx = x_func(u, v)
     np.rint(xx, out=xx)
@@ -140,31 +81,42 @@ def get_square_image(gray, width_pixels, height_pixels, points):
     yy = yy.astype(np.int)
     return gray[yy, xx]
 
+_TEST_POINTS = [
+    np.array([1,-2]),
+    np.array([10,-3]),
+    np.array([12,-9]),
+    np.array([2, -7]),
+    np.array([1,-2]),
+]
 
-def test_map_uv_to_xy():
-    import matplotlib.pyplot as plt
-    points = [
-        np.array([1,-2]),
-        np.array([10,-3]),
-        np.array([12,-9]),
-        np.array([2, -7]),
-        np.array([1,-2]),
-    ]
-    normals = find_normals(points)
-    # print(points)
-    print(normals)
-    # print(np.linalg.norm(normals, axis=1))
+def test_normals():
+    normals = find_normals(_TEST_POINTS)
+    # make sure they are unit vectors
     np.testing.assert_array_almost_equal(np.linalg.norm(normals, axis=1), np.ones(4))
-    # slope = -1/9
-    print(np.array([9, 1])/math.sqrt(82))
-    # assert normals[0] == np.array([9, 1])/math.sqrt(82)
-    plt.plot(*zip(*points), color='k')
-    for i in range(4):
-        p = (points[i] + points[(i+1)%4])/2
-        normal = normals[(i+1)%4]
-        # normal = normals[i]
-        plt.plot(*zip(*[p, p+normal]),label=f'n{(i+1)%4}')
-        # plt.plot(*zip(*[p, p+normal]),label=f'n{i}')
+    # n1, slope = -1/9
+    np.testing.assert_array_almost_equal((np.array([-1, -9])/math.sqrt(9*9+1*1)), normals[1])
+    # n2, slope = -6/2
+    np.testing.assert_array_almost_equal((np.array([-6, -2])/math.sqrt(2*2+6*6)), normals[2])
+    # n3, slope = 2/-10
+    np.testing.assert_array_almost_equal((np.array([2, 10])/math.sqrt(2*2+10*10)), normals[3])
+    # n3, slope = 5/-1
+    np.testing.assert_array_almost_equal((np.array([5, 1])/math.sqrt(1*1+5*5)), normals[0])
+
+
+def test_to_xy():
+    # visual inspection
+    points = _TEST_POINTS
+    normals = find_normals(_TEST_POINTS)
+    debug_plot = False
+    if debug_plot:
+        import matplotlib.pyplot as plt
+        plt.plot(*zip(*points), color='k')
+        for i in range(4):
+            p = (points[i] + points[(i+1)%4])/2
+            normal = normals[(i+1)%4]
+            plt.plot(*zip(*[p, p+normal]),label=f'n{(i+1)%4}')
+
+    poly = Polygon(points)
 
     for u in np.linspace(0,1,10):
         for v in np.linspace(0,1,10):
@@ -175,20 +127,24 @@ def test_map_uv_to_xy():
             c = (p - points[0])@normals[1]
             d = (p - points[3])@normals[3]
             _v = c / (c+d)
-            # print(u, _u)
-            # print(v, _v)
-            # print('#######')
+            np.testing.assert_almost_equal(_v, v)
+            np.testing.assert_almost_equal(_u, u)
+            if u != 0 and u != 1 and v!=0 and v!=1:
+                assert poly.contains(Point(x,y))
+            if debug_plot:
+                plt.plot([x], [y], marker='x', color='r')
+    if debug_plot:
+        plt.xlim([0,15])
+        plt.ylim([-15, 0])
+        plt.legend()
+        plt.show()
 
-            plt.plot([x], [y], marker='x', color='r')
-    plt.xlim([0,15])
-    plt.ylim([-15, 0])
-    plt.legend()
-    plt.show()
-    # print(map)
-    print(map_uv_to_xy(0, 0, points, normals))
-    print(map_uv_to_xy(0, 1, points, normals))
-    print(map_uv_to_xy(1, 0, points, normals))
-    print(map_uv_to_xy(1, 1, points, normals))
+    np.testing.assert_array_almost_equal(map_uv_to_xy(0, 0, points, normals), points[0])
+    np.testing.assert_array_almost_equal(map_uv_to_xy(0, 1, points, normals), points[3])
+    np.testing.assert_array_almost_equal(map_uv_to_xy(1, 0, points, normals), points[1])
+    np.testing.assert_array_almost_equal(map_uv_to_xy(1, 1, points, normals), points[2])
+
 
 if __name__ == '__main__':
-    test_map_uv_to_xy()
+    test_normals()
+    test_to_xy()
--- scan.py
+++ scan.py
@@ -41,50 +41,37 @@ def find_document(image):
     return document_corners
 
 def rectify_document(image, document_corners):
-    # transform to wide image (lots of unncecessary flipping to come!)
-    # doc_poly = Polygon(document_corners)
-    # if doc_poly.bounds[1] > doc_poly.bounds[0]:
-    #     # assert False
-    #     # image = np.fliplr(image.T)
-    #     fliplr = True
-    #     image = image.T
-    #     document_corners = np.array([[b,a] for a, b in document_corners])
-    # else:
-    #     fliplr = False
-    # make sure corners are ordered clockwise
+    assert document_corners.shape == (5,2)
+    # make sure corners are ordered clockwise, assumed in get_square_image
     doc_poly = Polygon(document_corners)
     if doc_poly.exterior.is_ccw:
         document_corners = np.array(list(map(list, reversed(document_corners))))
-    assert document_corners.shape == (5,2)
 
     # estimate dpi and use that to determine width and height
     doc_area = doc_poly.area
     dpi = math.sqrt(doc_area/(8.5*11))
-    # deltas =  document_corners[1:] - document_corners[:-1]
+
+    # determine if the found coordinate system has the image as "thin" or "wide"
     u_mag1 = np.linalg.norm(document_corners[1] - document_corners[0])
     v_mag1 = np.linalg.norm(document_corners[2] - document_corners[1])
     u_mag2 = np.linalg.norm(document_corners[3] - document_corners[2])
     v_mag2 = np.linalg.norm(document_corners[0] - document_corners[3])
     u_mag = (u_mag1+u_mag2)/2
     v_mag = (v_mag1+v_mag2)/2
-    # wide
-    # print(document_corners)
-    # print(delta)
-    # if abs(delta[1]) > abs(delta[0]):
-    if u_mag > v_mag:
-        # print('wide')
+    if u_mag > v_mag: # wide
         width_pixels = int(dpi * 11)
         height_pixels = int(dpi * 8.5)
-    else:
+    else: # thin
         width_pixels = int(dpi * 8.5)
         height_pixels = int(dpi * 11)
 
     rectified_image = rectification_utils.get_square_image(image, width_pixels, height_pixels, document_corners)
+    # transform to thin image, might be upside down
     if rectified_image.shape[0] < rectified_image.shape[1]:
         rectified_image = rectified_image.T
-        # rectified_image = np.fliplr(rectified_image)
 
     return rectified_image
+
 
 def threshold_image(image):
     # scale filters based on estimated pixel density
@@ -121,8 +108,7 @@ def main(image_file, outfile=None):
     document_corners = find_document(downsampled_image)
     # append first point as last to make a closed shape
     document_corners = np.array([list(xy) for xy in document_corners] + [list(document_corners[0])])
-    print(document_corners)
-    print(decimation_factor*document_corners)
+
     if PLOT_EXTRACTED_DOCUMENT:
         plt.imshow(image, cmap='gray', vmin=0, vmax=255)
         x, y = zip(*(decimation_factor*document_corners))
@@ -133,8 +119,6 @@ def main(image_file, outfile=None):
 
     # extract document into an approximate rectangle
     document_image = rectify_document(image, decimation_factor*document_corners)
-    # plt.imshow(document_image, cmap='gray', vmin=0, vmax=255)
-    # plt.show()
 
     # threshold into a "scanned" image
     document_image = threshold_image(document_image)
@@ -145,5 +129,7 @@ def main(image_file, outfile=None):
         import matplotlib.pyplot as plt 
         plt.imshow(image, cmap='gray', vmin=0, vmax=255)
         plt.show()
+
+
 if __name__ == '__main__':
     fire.Fire(main)
